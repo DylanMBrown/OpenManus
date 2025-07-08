b09a4264eca48b33aec4ed4c50fd8eeec0b43a84
DylanB <101678932+DylanMBrown@users.noreply.github.com>
2025-07-09 09:13:17 +1000

    Fix reserved keyword 'interface' in generate-qr.js

--- generate-qr.js
+++ generate-qr.js
@@ -6,10 +6,10 @@ function getLocalIP() {
   const interfaces = os.networkInterfaces();
   
   for (const name of Object.keys(interfaces)) {
-    for (const interface of interfaces[name]) {
+    for (const networkInterface of interfaces[name]) {
       // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
-      if (interface.family === 'IPv4' && !interface.internal) {
-        return interface.address;
+      if (networkInterface.family === 'IPv4' && !networkInterface.internal) {
+        return networkInterface.address;
       }
     }
   }
